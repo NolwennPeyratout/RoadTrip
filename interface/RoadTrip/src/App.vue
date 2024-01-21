8aa39dbb92773ae10f890aed3cbeacc37181b312
Timothée Poulain <nolwenn.peyratout@etu.univ-cotedazur.fr>
2024-01-21 21:23:26 +0100

    Base for our interface

--- interface/RoadTrip/src/App.vue
+++ interface/RoadTrip/src/App.vue
@@ -1,19 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Question from './components/Question.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <h1>Road Trip Planner</h1>
+    <Question />
   </main>
 </template>
 
---
+++ interface/RoadTrip/src/components/Question.vue
@@ -0,0 +1,94 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+function planRoadTrip() {
+    var country = document.getElementById("country").value;
+    var travelType = document.getElementById("travelType").value;
+    var situation = document.getElementById("situation").value;
+
+    var result = "You have chosen to plan a road trip in " + country + " with a " + travelType + " travel type, and your situation is " + situation + ".";
+    
+    document.getElementById("result").innerText = result;
+}
+</script>
+
+<template>
+    <div>
+  <div class="banner">
+    <div class="where">
+        <label for="country">Choose a Country:</label>
+        <select id="country">
+            <option value="UnitedStates">USA</option>
+            <option value="France">France</option>
+        </select>
+    </div>
+    <div class="howMany">
+        <label for="situation">Choose Your Situation:</label>
+        <select id="situation">
+            <option value="individual">Individual</option>
+            <option value="group">Group</option>
+        </select>
+    </div>
+    <div class="travelType">
+        <label for="travelType">Choose Travel Type:</label>
+        <select id="travelType">
+            <option value="rural">Rural</option>
+            <option value="urban">Urban</option>
+        </select>
+    </div>
+    <div class="research"><button @click="planRoadTrip">Plan Road Trip</button></div>
+  </div>
+  <div id="result"></div>
+</div>
+  
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+.banner{
+    display: table;
+    clear: both;
+    float:left;
+    height: 200px;
+    width: 100%;
+}
+.where{
+    float:left;
+    width: 30%;
+}
+.research{
+    float:left;
+    width: 10%;
+}
+.travelType{
+    float:left;
+    width: 30%;
+}
+.howMany{
+    float:left;
+    width: 30%;
+}
+
+
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
